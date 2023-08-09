b665029a6f5058d8af6d9da4e2868b09c5b4208c
Alisson de Moura <alisson_moura_rj@hotmail.com>
2023-08-08 21:51:57 -0300

    Calculo do Homem

--- src/cadastro/Cadastro.java
+++ src/cadastro/Cadastro.java
@@ -3,102 +3,86 @@ package cadastro;
 import java.time.Instant;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
+
+import calculo.Calculo;
 
 public class Cadastro {
 
 	public static void main(String[] args) {
 
-		Scanner sc = new Scanner(System.in);
 		Locale.setDefault(Locale.UK);
+		Scanner sc = new Scanner(System.in);
+
+		List<Calculo> lista = new ArrayList<>();
+
+		Calculo calc = new Calculo();
 
 		System.out.print("Quantas pessoas ? ");
 		int quantidade = sc.nextInt();
 
-		double tbmhomem = 0, tbmmulher = 0;
-		double fator1 = tbmhomem;
-		double fator2 = tbmmulher;
-		int numero = 0;
-		String[] nome = new String[quantidade];
-		char[] genero = new char[quantidade];
-		double[] peso = new double[quantidade];
-		double[] altura = new double[quantidade];
-		int[] idade = new int[quantidade];
-
+		sc.nextLine();
 		for (int i = 0; i < quantidade; i++) {
 			System.out.printf("Digite o %dª nome: ", i + 1);
-			nome[i] = sc.next();
+			String nome = sc.nextLine();
 			System.out.print("Digite o Genero ( m / f ): ");
-			genero[i] = sc.next().charAt(0);
+			char genero = sc.next().charAt(0);
 			System.out.print("Digite o peso (Kg): ");
-			peso[i] = sc.nextDouble();
+			Integer peso = sc.nextInt();
 			System.out.print("Digite a altura (cm): ");
-			altura[i] = sc.nextDouble();
+			Double altura = sc.nextDouble();
 			System.out.print("Digite a idade: ");
-			idade[i] = sc.nextInt();
-
-			if (genero[i] == 'm') {
-				tbmhomem = 66 + (13.8 * peso[i]) + (5 * altura[i]) - (6.8 * idade[i]);
-				System.out.println();
-				System.out.println("Nivel de Atividade fisica:");
-				System.out.println("1 - Sedentários (pouco ou nenhum exercício);");
-				System.out.println("2 - Levemente ativo (exercício leve 1 a 3 dias por semana);");
-				System.out.println("3 - Moderadamente ativo (exercício moderado, faz esportes 3 a 5 dias por semana);");
-				System.out.println("4 - Altamente ativo (exercício pesado de 5 a 6 dias por semana);");
-				System.out.println("5 - Extremamente ativo (exercício pesado diariamente e até 2 vezes por dia)");
-				System.out.println();
-				System.out.print("Escolha um numero: ");
-				numero = sc.nextInt();
-			}if (genero[i] == 'm') {
-				if (numero == 1) {
-					fator1 = tbmhomem * 1.2;
-				} else if (numero == 2) {
-					fator1 = tbmhomem * 1.375;
-				} else if (numero == 3) {
-					fator1 = tbmhomem * 1.55;
-				} else if (numero == 4) {
-					fator1 = tbmhomem * 1.725;
-				} else if (numero == 5) {
-					fator1 = tbmhomem * 1.9;
-					
-				}System.out.printf("Taxa de metabolismo Basal de %s: %.2f Kcal%n", nome[i], fator1);
-				System.out.println();
-			}else if (genero[i] == 'f') {
-					tbmmulher = 655 + (9.6 * peso[i]) + (1.8 * altura[i]) - (4.7 * idade[i]);
-					System.out.println();
-					System.out.println("Nivel de Atividade fisica:");
-					System.out.println("1 - Sedentários (pouco ou nenhum exercício);");
-					System.out.println("2 - Levemente ativo (exercício leve 1 a 3 dias por semana);");
-					System.out.println("3 - Moderadamente ativo (exercício moderado, faz esportes 3 a 5 dias por semana);");
-					System.out.println("4 - Altamente ativo (exercício pesado de 5 a 6 dias por semana);");
-					System.out.println("5 - Extremamente ativo (exercício pesado diariamente e até 2 vezes por dia)");
-					System.out.println();
-					System.out.print("Escolha um numero: ");
-					numero = sc.nextInt();
-				}if (genero[i] == 'f') {
-					if (numero == 1) {
-						fator2 = tbmmulher * 1.2;
-					} else if (numero == 2) {
-						fator2 = tbmmulher * 1.375;
-					} else if (numero == 3) {
-						fator2 = tbmmulher * 1.55;
-					} else if (numero == 4) {
-						fator2 = tbmmulher * 1.725;
-					} else if (numero == 5) {
-						fator2 = tbmmulher * 1.9;
-						
-					} System.out.printf("Taxa de metabolismo Basal de %s: %.2f Kcal%n", nome[i], fator2);
-				}
-			}
+			Integer idade = sc.nextInt();
 			System.out.println();
-			
-			DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy - HH:mm ").withZone(ZoneId.systemDefault());
-							//Quando for instanciar uma data com "INSTANT" utiliza - se ".withZone(ZoneId.systemDefault()"
+
+			if (genero == 'm') {
+				System.out.println(calc.toString1());
+				System.out.print("Escolha um numero: ");
+				Integer fator = sc.nextInt();
+
+				Calculo calculo = new Calculo(nome, genero, peso, altura, idade, fator);
+				lista.add(calculo);
+			}
+		}
+			for (Calculo listas : lista) {
+				System.out.println(listas.toString());
+			}
+			/*
+			 * System.out.printf("Taxa de metabolismo Basal de %s: %.2f Kcal%n", nome[i],
+			 * fator1); System.out.println(); } else if (genero[i] == 'f') { tbmmulher = 655
+			 * + (9.6 * peso[i]) + (1.8 * altura[i]) - (4.7 * idade[i]);
+			 * System.out.println(); System.out.println("Nivel de Atividade fisica:");
+			 * System.out.println("1 - Sedentários (pouco ou nenhum exercício);");
+			 * System.out.
+			 * println("2 - Levemente ativo (exercício leve 1 a 3 dias por semana);");
+			 * System.out.
+			 * println("3 - Moderadamente ativo (exercício moderado, faz esportes 3 a 5 dias por semana);"
+			 * ); System.out.
+			 * println("4 - Altamente ativo (exercício pesado de 5 a 6 dias por semana);");
+			 * System.out.
+			 * println("5 - Extremamente ativo (exercício pesado diariamente e até 2 vezes por dia)"
+			 * ); System.out.println(); System.out.print("Escolha um numero: "); numero =
+			 * sc.nextInt(); } if (genero[i] == 'f') { if (numero == 1) { fator2 = tbmmulher
+			 * * 1.2; } else if (numero == 2) { fator2 = tbmmulher * 1.375; } else if
+			 * (numero == 3) { fator2 = tbmmulher * 1.55; } else if (numero == 4) { fator2 =
+			 * tbmmulher * 1.725; } else if (numero == 5) { fator2 = tbmmulher * 1.9;
+			 * 
+			 * } System.out.printf("Taxa de metabolismo Basal de %s: %.2f Kcal%n", nome[i],
+			 * fator2); } } System.out.println();
+			 */
+			System.out.println();
+			DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy - HH:mm ")
+					.withZone(ZoneId.systemDefault());
+			// Quando for instanciar uma data com "INSTANT" utiliza - se
+			// ".withZone(ZoneId.systemDefault()"
 			Instant hora = Instant.now();
 			System.out.println("Data da Consulta: " + formato.format(hora));
+
+			
 		
 		sc.close();
 	}
 }
-	
--- src/calculo/Calculo.java
+++ src/calculo/Calculo.java
@@ -2,8 +2,95 @@ package calculo;
 
 public class Calculo {
 
-	
-	
-	
-	
+	private String nome;
+	private char genero;
+	private Integer peso;
+	private Double altura;
+	private Integer idade;
+	private Integer fator;
+
+	public Calculo(String nome, char genero, Integer peso, Double altura, Integer idade, Integer fator) {
+		super();
+		this.nome = nome;
+		this.genero = genero;
+		this.peso = peso;
+		this.altura = altura;
+		this.idade = idade;
+		this.fator = fator;
+	}
+
+	public Calculo() {
+		super();
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public char getGenero() {
+		return genero;
+	}
+
+	public void setGenero(char genero) {
+		this.genero = genero;
+	}
+
+	public Integer getPeso() {
+		return peso;
+	}
+
+	public void setPeso(Integer peso) {
+		this.peso = peso;
+	}
+
+	public Double getAltura() {
+		return altura;
+	}
+
+	public void setAltura(Double altura) {
+		this.altura = altura;
+	}
+
+	public Integer getIdade() {
+		return idade;
+	}
+
+	public void setIdade(Integer idade) {
+		this.idade = idade;
+	}
+
+	public Double calculohomem() {
+
+		if (fator == 1) {
+			return (66 + (13.8 * peso) + (5 * altura) - (6.8 * idade)) * 1.2;
+		} else if (fator == 2) {
+			return (66 + (13.8 * peso) + (5 * altura) - (6.8 * idade)) * 1.375;
+		} else if (fator == 3) {
+			return (66 + (13.8 * peso) + (5 * altura) - (6.8 * idade)) * 1.55;
+		} else if (fator == 4) {
+			return (66 + (13.8 * peso) + (5 * altura) - (6.8 * idade)) * 1.725;
+		} else if (fator == 5) {
+			return (66 + (13.8 * peso) + (5 * altura) - (6.8 * idade)) * 1.9;
+		}
+		return null;
+	}
+
+	public String toString1() {
+		return "Nivel de Atividade fisica:\n" + "1 - Sedentários (pouco ou nenhum exercício);\n"
+				+ "2 - Levemente ativo (exercício leve 1 a 3 dias por semana);\n"
+				+ "3 - Moderadamente ativo (exercício moderado, faz esportes 3 a 5 dias por semana);\n"
+				+ "4 - Altamente ativo (exercício pesado de 5 a 6 dias por semana);\n"
+				+ "5 - Extremamente ativo (exercício pesado diariamente e até 2 vezes por dia)\n";
+	}
+
+	public String toString() {
+		return "Nome: " 
+				+ nome + " - " 
+				+ String.format("%.2f", calculohomem()) 
+				+ " Kcal";
+	}
 }
